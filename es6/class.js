478b3f3efd16c5d8bb4eab4a7b438282f7999c93
rutvi <rutvis@webelight.co.in>
2022-02-24 13:00:16 +0530

    class file

---
+++ es6/class.js
@@ -0,0 +1,22 @@
+class Employee{
+    constructor(givenName , givenExperience, givenDivision){
+        this.name = givenName;
+        this.experience = givenExperience; 
+        this.division = givenDivision;
+    }
+    slogan(){
+        return `I am ${this.name} and this cpmpany is the best.`;
+    }
+    joiningYear(){
+        return 2022 - this.experience;
+    }
+
+    static add(a, b){
+        return a + b;
+    }
+    
+}
+
+rutvi = new Employee("rutvi", "2", "Division");
+// console.log(rutvi.joiningYear());
+console.log(Employee.add(34,5))
