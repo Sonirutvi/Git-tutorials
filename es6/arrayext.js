58380e78834c525e9ab70136a4b6562374d2ca69
rutvi <rutvis@webelight.co.in>
2022-02-25 18:15:38 +0530

    array extension file

--- es6/arrayext.js
+++ es6/arrayext.js
@@ -4,34 +4,34 @@
 //Array.of()
 
 // let x = new Array(5) //es5
-// let x = Array.of(5) //es6 
-// console.log(x.length)
-// console.log(x[0])
+// let x = Array.of("5","6") //es6 
+// console.log(x.length) //length of string
+// console.log(x[0]) //index number
 
-//Array.from()
+// //Array.from()
 
 function abc(){
-    // console.log(arguments)
-    // let arr=[]
-    // for(let i of arguments){
-    //     arr.push(i)
-    // }
-    // return arr
+    console.log(arguments)
+    let arr=[]
+    for(let i of arguments){
+        arr.push(i)
+    }
+    return arr
     return Array.from(arguments)
 }
 
 let x=abc(3,4,5,6)
 console.log(x)
 
-//Array find()
+// //Array find()
 
 let arr=[1,2,3,4,5,6,7,10]
-// console.log(arr.indexOf(2))
+console.log(arr.indexOf(2))
 
 console.log(arr.find(e=>e%2===0))//first even number
 console.log(arr.findIndex(e=>e%2===0))//find index
 
-//Array findeIndex() for object
+// //Array findeIndex() for object
 
 let student = [{name:'xyz', age:27},
 {name:'xyz', age:28},
