d6a4038db08d3a802043c9e9d4e52cc457dc02ac
rutvi <rutvis@webelight.co.in>
2022-02-25 11:44:16 +0530

    arrowfunction file

---
+++ es6/arrowfunc.js
@@ -0,0 +1,12 @@
+//arrow function
+
+// let welcome = (name) => {
+//     return `hello & welcome ${name}`;
+// }
+ 
+// let welcome = (name) => `hello & welcome ${name}`;
+
+let welcome = name => `hello & welcome${name}`;
+
+console.log(welcome("rutvi"));
+
